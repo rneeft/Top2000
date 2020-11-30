05eb2837f545aef0ef5ee3df42fed1b97c43040f
Rick Neeft <rick@chroomsoft.nl>
2020-11-30 22:20:10 +0100

    init app

---
+++ src/apps/XamarinApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using XamarinApp.ViewModels;
+
+namespace XamarinApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
