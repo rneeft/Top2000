2b739487b2d45376e2cc43f927496b8ee0d0efd8
Rick <rick@chroomsoft.nl>
2020-12-26 14:26:37 +0100

    CSV Export (#26)

--- src/data/Data.ClientDatabase/ConfigureServices.cs
+++ src/data/Data.ClientDatabase/ConfigureServices.cs
@@ -26,5 +26,19 @@ namespace Chroomsoft.Top2000.Data.ClientDatabase
                     return new SQLiteAsyncConnection(databasePath, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create | SQLiteOpenFlags.SharedCache, storeDateTimeAsTicks: false);
                 });
         }
+
+        public static IServiceCollection AddOfflineClientDatabase(this IServiceCollection services, DirectoryInfo appDataDirectory, string clientDatabaseName = "Top2000v2.db")
+        {
+            return services
+                .AddTransient<Top2000AssemblyDataSource>()
+                .AddTransient<IUpdateClientDatabase, UpdateDatabase>()
+                .AddTransient<ITop2000AssemblyData, Top2000Data>()
+                .AddTransient<SQLiteAsyncConnection>(f =>
+                {
+                    var databasePath = Path.Combine(appDataDirectory.FullName, clientDatabaseName);
+
+                    return new SQLiteAsyncConnection(databasePath, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create | SQLiteOpenFlags.SharedCache, storeDateTimeAsTicks: false);
+                });
+        }
     }
 }
