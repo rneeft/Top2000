e57f7760aea8de946427056e7881540a3253e3f1
Rick Neeft <rick@chroomsoft.nl>
2021-10-22 22:27:18 +0200

    Fix issue #33

--- src/data/Data.SQLScripts/sql/0048-FixListings.sql
+++ src/data/Data.SQLScripts/sql/0048-FixListings.sql
@@ -1,4 +1,12 @@
 ﻿-- Merge duplicate listing: Biko - Peter Gabriel.
-UPDATE Listing SET TrackId = 347 WHERE TrackId = 3916
-DELETE Track where Id = 3916
+UPDATE Listing 
+SET TrackId = 347 
+WHERE TrackId = 3916;
+
+DELETE Track WHERE Id = 3916;
+
+-- School Supertramp is in the wrong play time.
+UPDATE Listing 
+SET [PlayUtcDateAndTime] = '2020-12-31T17:00:00' 
+WHERE TrackId = 2601 AND Edition = 2020;
 
