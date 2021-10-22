487101e2d08158355df16aa0513049d7146699a0
Rick Neeft <rick@chroomsoft.nl>
2021-10-23 00:18:07 +0200

    Fix issue with "More than a woman" van de 'Bee Gees'

--- src/data/Data.SQLScripts/sql/0048-FixListings.sql
+++ src/data/Data.SQLScripts/sql/0048-FixListings.sql
@@ -10,3 +10,13 @@ UPDATE Listing
 SET [PlayUtcDateAndTime] = '2020-12-31T17:00:00' 
 WHERE TrackId = 2601 AND Edition = 2020;
 
+-- Het nummer "More than a woman" in de Top2000 lijst van 2019 en 2020 is van de Bee Gees, niet van Tavares.
+INSERT INTO Track(Id, Title, Artist, RecordedYear) VALUES (4588, 'More than a woman', 'Bee Gees',  1977);
+UPDATE Listing 
+SET TrackId = 4588 
+WHERE TrackId = 2081 AND Edition = 2019
+
+UPDATE Listing 
+SET TrackId = 4588 
+WHERE TrackId = 2081 AND Edition = 2020
+
