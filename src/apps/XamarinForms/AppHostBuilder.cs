49b6724bb5caa763e25a8ed33205365d51ee1a7a
Rick Neeft <rick@chroomsoft.nl>
2021-12-18 22:36:36 +0100

    Android version to 2.4.0 and version number to 30

--- src/apps/XamarinForms/AppHostBuilder.cs
+++ src/apps/XamarinForms/AppHostBuilder.cs
@@ -89,8 +89,8 @@ namespace Chroomsoft.Top2000.Apps
         {
             var current = DateTime.UtcNow;
 
-            var first = new DateTime(current.Year, 12, 24, 23, 0, 0, DateTimeKind.Utc); // first day of Christmas
-            var last = new DateTime(current.Year, 12, 31, 23, 0, 0, DateTimeKind.Utc); // new year
+            var first = new DateTime(current.Year, 12, 24, 23, 0, 0, DateTimeKind.Utc); // first day of Christmas for CET in UTC time
+            var last = new DateTime(current.Year, 12, 31, 23, 0, 0, DateTimeKind.Utc); // new year for CET in UTC time
 
             return (current > first && current < last);
         }
